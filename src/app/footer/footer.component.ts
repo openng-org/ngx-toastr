23267228e029e03f612b3a681b0ff7c27f96f3dc
Jason Weinzierl <weinzierljason@gmail.com>
2026-08-10 15:17:57 -0500

    chore: default branch was renamed to main

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,7 +6,7 @@ import { Component, VERSION } from '@angular/core';
     <footer class="footer mb-4 mt-5">
       Angular {{ version }}
       <br />
-      <a href="https://github.com/openng-org/ngx-toastr/blob/master/LICENSE">MIT license</a>
+      <a href="https://github.com/openng-org/ngx-toastr/blob/main/LICENSE">MIT license</a>
       -
       <a href="https://github.com/openng-org/ngx-toastr">Source</a>
     </footer>
