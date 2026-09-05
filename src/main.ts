b13267834b328f8046242018796c32b54dbef01c
Jason Weinzierl <weinzierljason@gmail.com>
2026-09-05 15:03:14 -0500

    chore: run demo app and tests as zoneless

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideToastr } from '@openng/ngx-toastr';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideBrowserGlobalErrorListeners(), provideToastr()],
+  providers: [
+    provideZonelessChangeDetection(),
+    provideBrowserGlobalErrorListeners(),
+    provideToastr(),
+  ],
 }).catch(err => console.error(err));
